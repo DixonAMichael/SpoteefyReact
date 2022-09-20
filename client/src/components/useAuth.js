ee43a882692471b1c0efe31fd4297bca72bd34c9
DixonAMichael <dixonamichael@gmail.com>
2022-09-20 13:50:53 -0400

    created post route for refresh token

--- client/src/components/useAuth.js
+++ client/src/components/useAuth.js
@@ -7,20 +7,25 @@ export default function useAuth(code) {
     const [expiresIn, setExpiresIn] = useState()
 
     useEffect(() => {
-        axios.post('http://localhost:3001/login', {
-            code,
-        }).then(res => {
-            setAccessToken(res.data.accessToken)
-            setRefreshToken(res.data.refreshToken)
-            setExpiresIn(res.data.expiresIn)
-            window.history.pushState({}, null, '/')
-        }).catch((error) => {
-            console.log(error)
-            //window.location = "/"
-        })
+        axios
+            .post("http://localhost:3001/login", {
+                code,
+            })
+            .then(res => {
+                setAccessToken(res.data.accessToken)
+                setRefreshToken(res.data.refreshToken)
+                setExpiresIn(res.data.expiresIn)
+                window.history.pushState({}, null, '/')
+            }).catch(() => {
+                window.location = "/"
+            })
     }, [code])
 
-    useEffect(() => { }, [refreshToken, expiresIn])
+    useEffect(() => {
+    
+     }, [refreshToken, expiresIn])
+
+
 
     return accessToken
-};
+}
--- server/server.js
+++ server/server.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const spotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new spotifyWebApi({
+        redirectUri: 'http://localhost:3000/',
+        clientId: 'a097fe731b9c4be48ab93d3d005a07d0',
+        clientSecret: '84c88e882d624f25b842340bba1d25f0',
+        refreshToken
+})
+spotifyApi.refreshAccessToken().then(
+    (data) => {
+      console.log('The access token has been refreshed!');
+  
+      // Save the access token so that it's used in future calls
+      spotifyApi.setAccessToken(data.body['access_token']);
+    }).catch(() => {
+        res.sendStatus(400)
+    })
+})
+
 
 app.post('/login', (req, res) => {
     const code = req.body.code
