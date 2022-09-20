e81bb3a8d18cc8c68f64803e5500be32365ff16f
DixonAMichael <dixonamichael@gmail.com>
2022-09-20 16:17:38 -0400

    at a stopping point

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import { useState } from 'react';
 import useAuth from './useAuth';
+import { Container, Form } from 'react-bootstrap';
 
 export default function Dashboard({ code }) {
     const accessToken = useAuth(code)
-    //console.log(code)
-    return <div>{code}</div>
+    const [search, setSearch] = useState("")
+    return <Container className='d-flex flex-column py-2' style={{height: '100vh'}}>
+        <Form.Control type="search" placeholder="Search Songs/Artists" 
+        value={search} onChange={e => setSearch(e.target.value)} />
+    </Container>
 }
