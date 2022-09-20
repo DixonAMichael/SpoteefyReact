222577f110e059271877caa44726a444a06fcaa1
DixonAMichael <dixonamichael@gmail.com>
2022-09-20 12:47:14 -0400

    install axios

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,11 @@ import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 
 const code = new URLSearchParams(window.location.search).get('code');
+// gets the URL param 'code='.
 console.log(code);
 
 function App() {
   return code ? <Dashboard code={code} /> : <Login />
 }
-
+// if code is found, renders dashboard component otherwise renders login component.
 export default App;
---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const spotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+
+app.post('/login', (req, res) => {
+    const code = req.body.code
+    const spotifyApi = new spotifyWebApi({
+        redirectUri: 'http://localhost:3000/',
+        clientId: 'a097fe731b9c4be48ab93d3d005a07d0',
+        clientSecret: '84c88e882d624f25b842340bba1d25f0',
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            //get access token.
+            refreshToken: data.body.refresh_token,
+            // gets refresh token.
+            expiresIn: data.body.expires_in
+            // set time limit on access token.
+        })
+    }) .catch(() => {
+        res.sendStatus(400)
+    });
+});
