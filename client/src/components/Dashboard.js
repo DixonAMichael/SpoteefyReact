9d1ea0dec580a1fd1135263ced0d4f8faa65b489
DixonAMichael <dixonamichael@gmail.com>
2022-09-21 09:36:05 -0400

    create search result component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,8 +5,12 @@ import { Container, Form } from 'react-bootstrap';
 export default function Dashboard({ code }) {
     const accessToken = useAuth(code)
     const [search, setSearch] = useState("")
-    return <Container className='d-flex flex-column py-2' style={{height: '100vh'}}>
-        <Form.Control type="search" placeholder="Search Songs/Artists" 
-        value={search} onChange={e => setSearch(e.target.value)} />
-    </Container>
-}
+    return( 
+    <Container className='d-flex flex-column py-2' style={{height: '100vh'}}>
+        <Form.Control 
+        type="search" 
+        placeholder="Search Songs/Artists" 
+        value={search} 
+        onChange={e => setSearch(e.target.value)} />
+
+</Container> )};
