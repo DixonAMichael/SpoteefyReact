01d5d1c0ab805f934c0ad8596102866439477557
DixonAMichael <dixonamichael@gmail.com>
2022-09-23 10:48:59 -0400

    spotify-web-api-node no longer works

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -8,7 +8,7 @@ import { Container } from 'react-bootstrap';
 // Streaming scope allows application see see what user is currently listening to.
 // User-read-email and User-read-private are requird scopes to access Spotify user information.
 // user-library-read determines if a song is in the user library.
-// user-library-modify allows uuser to add and remove songs from library.
+// user-library-modify allows user to add and remove songs from library.
 // user-read-playback-state allows client to see if user is currently playing a song and what song it is.
 // user-modify-playback-state allows user to play, pause, skip and repeat current track.
 const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=a097fe731b9c4be48ab93d3d005a07d0&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
